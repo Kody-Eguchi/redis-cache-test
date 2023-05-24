17bba86dfd66ac29ded040acaf6377cb250daf4e
Koji Eguchi <koji.eguchi@hotmail.com>
2023-05-24 10:10:49 -0400

    Add reusable function getOrSetCache

--- server.js
+++ server.js
@@ -14,32 +14,60 @@ app.use(cors());
 
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId;
-
-  const photos = await redisClient.get("photos");
-
-  if (photos != null) {
-    console.log("Cache Hit");
-    return res.json(JSON.parse(photos));
-  } else {
-    console.log("Cache Miss");
+  const photos = await getOrSetCache(`photos?albumId=${albumId}`, async () => {
     const { data } = await axios.get(
       "https://jsonplaceholder.typicode.com/photos",
       { params: { albumId } }
     );
-    await redisClient.setEx("photos", EXPIRATION, JSON.stringify(data));
-    res.json(data);
-  }
+    return data;
+  });
+  res.json(photos);
 });
+
+// app.get("/photos", async (req, res) => {
+//   const albumId = req.query.albumId;
+
+//   const photos = await redisClient.get(`photos?albumId=${albumId}`);
+
+//   if (photos != null) {
+//     console.log("Cache Hit");
+//     return res.json(JSON.parse(photos));
+//   } else {
+//     console.log("Cache Miss");
+//     const { data } = await axios.get(
+//       "https://jsonplaceholder.typicode.com/photos",
+//       { params: { albumId } }
+//     );
+//     await redisClient.setEx(
+//       `photos?albumId=${albumId}`,
+//       EXPIRATION,
+//       JSON.stringify(data)
+//     );
+//     res.json(data);
+//   }
+// });
 
 app.get("/photos/:id", async (req, res) => {
-  const albumId = req.query.albumId;
-  const { data } = await axios.get(
-    `https://jsonplaceholder.typicode.com/photos/${req.params.id}`,
-    { params: { albumId } }
-  );
+  const photo = await getOrSetCache(`photos:${req.params.id}`, async () => {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+    );
+    return data;
+  });
 
-  res.json(data);
+  res.json(photo);
 });
+
+function getOrSetCache(key, callback) {
+  return new Promise(async (resolve, reject) => {
+    const data = await redisClient.get(key);
+
+    if (data != null) return resolve(JSON.parse(data));
+    const freshData = await callback();
+    redisClient.setEx(key, EXPIRATION, JSON.stringify(freshData));
+    resolve(freshData);
+  });
+}
 
 app.listen(3000, () => {
   console.log("Listening on Port 3000");
